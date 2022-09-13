91cf6be4b7aed4ac9f8da1751b69e6c01d9f19d7
hkreds <109258421+hkreds@users.noreply.github.com>
2022-09-13 09:52:03 +0000

    [Add]フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -29,6 +30,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
       render :edit
