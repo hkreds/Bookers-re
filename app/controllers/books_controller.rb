6df3cd447829d0b6bd46cf0ff61b96421e45d1fa
hkreds <109258421+hkreds@users.noreply.github.com>
2022-09-13 08:59:06 +0000

    [Add]bookの投稿フォームとnew・createアクション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,16 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path
+  end
 
   def edit
   end
