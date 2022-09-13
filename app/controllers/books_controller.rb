d258b31b89b23c2b4151ab831f0916126adbb4c1
hkreds <109258421+hkreds@users.noreply.github.com>
2022-09-13 09:46:53 +0000

    [Add]バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def new
@@ -9,8 +10,12 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
@@ -23,8 +28,11 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
